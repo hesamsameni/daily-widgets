d8fdb1f77fd18a1290ab3a589e2fc3f64f9b35e1
Hesam Sameni <hesam.sameni@gmail.com>
2021-02-03 00:12:45 +0330

    adding new vue

---
+++ client/src/views/Register.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h1>Register</h1>
+    <input type="email" placeholder="email" v-model="email" />
+    <input type="password" placeholder="password" v-model="password" />
+    <button @click="register">Register</button>
+  </div>
+</template>
+
+<script>
+import AuthenticationService from '@/services/AuthenticationService'
+export default {
+  name: 'Register',
+  data() {
+    return {
+      email: '',
+      password: '',
+    }
+  },
+  methods: {
+    async register() {
+      const response = await AuthenticationService.register({
+        email: this.email,
+        password: this.password,
+      })
+      console.log(response.data)
+    },
+  },
+}
+</script>
+
+<style scoped></style>
